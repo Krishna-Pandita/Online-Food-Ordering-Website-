e726fdc54d73ec701a75224fc85a79cec8689df3
Krishna-Pandita <kpandita68@gmail.com>
2026-01-08 19:53:42 +0530

    Add hover colors to navigation buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { useState } from "react"
 import {Link} from "react-router-dom";
 import useOnlinestatus from "../utils/useonlinestatus";
 
+
 const Header = () => {
   const [btnNameReact, setbtnNameReact] = useState("Login");
   const isOnline = useOnlinestatus();
@@ -11,7 +12,7 @@ const Header = () => {
 
   return (
 
-    <div className="flex border-solid border-2 justify-between h-40 bg-blue-100 px-8">                             {/* Full Header Section*/}
+    <div className="flex justify-between h-40 px-8">                             {/* Full Header Section*/}
 
       <div className="flex items-center">                    {/* Logo Section*/}
 
@@ -21,12 +22,12 @@ const Header = () => {
 
       <div className="flex items-center justify-center">                           {/* Navbar Section*/}
         <ul className="flex justify-center items-center gap-12 text-[29px] font-normal ">
-          <li id="onlinestatus">Online Status: {isOnline ? "✅" : "🔴"}</li>
-          <li> <Link to="/">Home</Link></li>
-          <li> <Link to="/about">About Us</Link></li>
-          <li><Link to="/contact">Contact us</Link></li>
-          <li><Link to="/grocery">Grocery</Link></li>
-          <li>Cart</li>
+          <li id="onlinestatus">Online Status: {isOnline ? "Active" : "Disconnected"}</li>
+          <li className="hover:text-orange-500"> <Link to="/">Home</Link></li>
+          <li className="hover:text-orange-500"> <Link to="/about">About Us</Link></li>
+          <li className="hover:text-orange-500"> <Link to="/contact">Contact us</Link></li>
+          <li className="hover:text-orange-500"> <Link to="/grocery">Grocery</Link></li>
+          <li className="hover:text-orange-500">Cart</li>
         </ul>
       </div>
 
