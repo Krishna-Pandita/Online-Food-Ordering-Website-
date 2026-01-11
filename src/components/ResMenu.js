f154c065183b31dd3266b3b7a1899db0c016a433
Krishna-Pandita <kpandita68@gmail.com>
2026-01-11 17:33:54 +0530

    added Restaurant image and style in resMenu

--- src/components/ResMenu.js
+++ src/components/ResMenu.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import resobj from "../utils/mockdata";
+import { CDN_URL } from "../utils/constants";
 import useResinfo from "../utils/useResinfo";
 
 const ResMenu = () => {
@@ -19,16 +20,21 @@ const ResMenu = () => {
     costForTwo,
     avgRating,
     cloudinaryImageId,
+    sla,
   } = restaurant.info;
 
   return (
-    <div className="menu">
-      <img src={cloudinaryImageId} alt="Image Not found"></img>
-      <h1>{name}</h1>
-      <h3>{cuisines.join(", ")}</h3>
-      <h3>{costForTwo}</h3>
-      <h3>Rating: {avgRating}</h3>
-    </div>
+    <div className=" my-7 mx-9">
+      <div className="resMenu-card border border-solid border-gray-400 rounded-2xl w-98 h-auto py-4 cursor-pointer transform hover:scale-95 transition-transform duration-300">
+      <img className="h-80 w-85 rounded-2xl mb-3 mx-6 " src={CDN_URL+cloudinaryImageId} alt="Image Not found"></img>
+      <h1 className="mx-6 py-1 font-bold text-2xl">{name}</h1>
+      <h3 className="mx-6 py-1 text-xl">{cuisines.join(", ")}</h3>
+      <h3 className="mx-6 py-1  text-xl">{costForTwo}</h3>
+      <h3 className="mx-5 py-1 px-1 text-xl">⭐ {avgRating}
+  <span>•</span>
+  {sla.slaString}</h3>
+  </div>
+      </div>
   );
 };
 
