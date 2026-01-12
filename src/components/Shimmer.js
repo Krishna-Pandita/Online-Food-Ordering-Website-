3a979861c1d85232b00a0708ba2cb2a015157917
Krishna-Pandita <kpandita68@gmail.com>
2026-01-12 13:09:05 +0530

    Add shimmer UI for loading state

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,16 +1,14 @@
 const Shimmer = () => {
-    return <div className="shimmer-container">
+    return <div className="my-9 mt-17 flex flex-wrap gap-7 justify-center items-center">
         
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
+        <div className="h-120 w-100 bg-gray-200 p-4 rounded-2xl"></div>
+        <div className="h-120 w-100 bg-gray-200 p-4 rounded-2xl"></div>
+        <div className="h-120 w-100 bg-gray-200 p-4 rounded-2xl"></div>
+        <div className="h-120 w-100 bg-gray-200 p-4 rounded-2xl"></div>
+        <div className="h-120 w-100 bg-gray-200 p-4 rounded-2xl"></div>
+        <div className="h-120 w-100 bg-gray-200 p-4 rounded-2xl"></div>
+        <div className="h-120 w-100 bg-gray-200 p-4 rounded-2xl"></div>
+        <div className="h-120 w-100 bg-gray-200 p-4 rounded-2xl"></div>
 
     </div>
 }
