894a57895a0eaa1eaef556e42ca1d5a432130e58
Krishna-Pandita <kpandita68@gmail.com>
2026-02-01 18:25:41 +0530

    Added more Restaurants data

--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -3746,7 +3746,575 @@ const resobj = [
       type: "WEBLINK"
     },
     widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
-  }
+  },
+
+  {
+    "info": {
+      "id": "195429",
+      "name": "Sanju Ka Dhaba",
+      "cloudinaryImageId": "hjao7sorzggaeqito6au",
+      "locality": "Vishnu Nagar",
+      "areaName": "Prasia Road",
+      "costForTwo": "₹250 for two",
+      "cuisines": [
+        "South Indian",
+        "Chinese"
+      ],
+      "avgRating": 4.6,
+      "veg": true,
+      "parentId": "177443",
+      "avgRatingString": "4.6",
+      "totalRatingsString": "1.3K+",
+      "sla": {
+        "deliveryTime": 55,
+        "lastMileTravel": 13.9,
+        "serviceability": "SERVICEABLE",
+        "slaString": "50-60 mins",
+        "lastMileTravelString": "13.9 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2026-01-25 22:30:00",
+        "opened": true
+      },
+      "badges": {
+        "imageBadges": [
+          {
+            "imageId": "v1695133679/badges/Pure_Veg111.png",
+            "description": "pureveg"
+          }
+        ]
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "pureveg",
+                  "imageId": "v1695133679/badges/Pure_Veg111.png"
+                }
+              }
+            ]
+          },
+          "textBased": {
+
+          },
+          "textExtendedBadges": {
+
+          }
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "lottie": {
+
+          },
+          "video": {
+
+          }
+        }
+      },
+      "reviewsSummary": {
+
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-7dab50aa-5d32-4721-a5e3-601110ee7d93"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/chhindwara/sanju-ka-dhaba-vishnu-nagar-prasia-road-rest195429",
+      "type": "WEBLINK"
+    }
+  },
+
+  {
+    "info": {
+      "id": "487096",
+      "name": "Dino's Pizza",
+      "cloudinaryImageId": "seqnlipynpingek2iirl",
+      "locality": "Teacher's Colony",
+      "areaName": "Parasia Road",
+      "costForTwo": "₹200 for two",
+      "cuisines": [
+        "Pizzas",
+        "Burgers",
+        "Beverages"
+      ],
+      "avgRating": 4.1,
+      "parentId": "387459",
+      "avgRatingString": "4.1",
+      "totalRatingsString": "277",
+      "sla": {
+        "deliveryTime": 68,
+        "lastMileTravel": 12.3,
+        "serviceability": "SERVICEABLE",
+        "slaString": "65-75 mins",
+        "lastMileTravelString": "12.3 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2026-01-25 23:45:00",
+        "opened": true
+      },
+      "badges": {
+        "imageBadges": [
+          {
+            "imageId": "v1695133679/badges/Pure_Veg111.png",
+            "description": "pureveg"
+          }
+        ]
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "pureveg",
+                  "imageId": "v1695133679/badges/Pure_Veg111.png"
+                }
+              }
+            ]
+          },
+          "textBased": {
+
+          },
+          "textExtendedBadges": {
+
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "₹150 OFF",
+        "subHeader": "ABOVE ₹349",
+        "discountTag": "FLAT DEAL"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "lottie": {
+
+          },
+          "video": {
+
+          }
+        }
+      },
+      "reviewsSummary": {
+
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-7dab50aa-5d32-4721-a5e3-601110ee7d93"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/chhindwara/dinos-pizza-teachers-colony-parasia-road-rest487096",
+      "type": "WEBLINK"
+    }
+  },
+
+  {
+    "info": {
+      "id": "213358",
+      "name": "Apni Rasoi Family Dhaba",
+      "cloudinaryImageId": "sidigb8zqjfrfpkrtqgl",
+      "locality": "Vishnu Nagar",
+      "areaName": "Parasia Road",
+      "costForTwo": "₹250 for two",
+      "cuisines": [
+        "Indian",
+        "South Indian",
+        "Chinese"
+      ],
+      "avgRating": 4.2,
+      "veg": true,
+      "parentId": "35024",
+      "avgRatingString": "4.2",
+      "totalRatingsString": "1.4K+",
+      "sla": {
+        "deliveryTime": 55,
+        "lastMileTravel": 14,
+        "serviceability": "SERVICEABLE",
+        "slaString": "55-65 mins",
+        "lastMileTravelString": "14.0 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2026-01-25 23:00:00",
+        "opened": true
+      },
+      "badges": {
+
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+
+          },
+          "textBased": {
+
+          },
+          "textExtendedBadges": {
+
+          }
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "lottie": {
+
+          },
+          "video": {
+
+          }
+        }
+      },
+      "reviewsSummary": {
+
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-7dab50aa-5d32-4721-a5e3-601110ee7d93"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/chhindwara/apni-rasoi-family-dhaba-vishnu-nagar-parasia-road-rest213358",
+      "type": "WEBLINK"
+    }
+  },
+
+  {
+    "info": {
+      "id": "150586",
+      "name": "Accord International",
+      "cloudinaryImageId": "ntcsuou2650qimnkrc3m",
+      "locality": "Parasia Road",
+      "areaName": "Vishnu Nagar",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "South Indian",
+        "Continental",
+        "Beverages"
+      ],
+      "avgRating": 4.4,
+      "parentId": "26828",
+      "avgRatingString": "4.4",
+      "totalRatingsString": "696",
+      "sla": {
+        "deliveryTime": 62,
+        "lastMileTravel": 11.6,
+        "serviceability": "SERVICEABLE",
+        "slaString": "60-70 mins",
+        "lastMileTravelString": "11.6 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2026-01-25 23:30:00",
+        "opened": true
+      },
+      "badges": {
+
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+
+          },
+          "textBased": {
+
+          },
+          "textExtendedBadges": {
+
+          }
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "lottie": {
+
+          },
+          "video": {
+
+          }
+        }
+      },
+      "reviewsSummary": {
+
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-7dab50aa-5d32-4721-a5e3-601110ee7d93"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/chhindwara/accord-international-parasia-road-vishnu-nagar-rest150586",
+      "type": "WEBLINK"
+    }
+  },
+
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+      "id": "614453",
+      "name": "Asha Hotel",
+      "cloudinaryImageId": "weyo2tdv2qsvimhkbgue",
+      "areaName": "Prasia Road",
+      "costForTwo": "₹200 for two",
+      "cuisines": [
+        "Snacks",
+        "Salads",
+        "Beverages"
+      ],
+      "avgRating": 4.4,
+      "parentId": "241321",
+      "avgRatingString": "4.4",
+      "totalRatingsString": "106",
+      "sla": {
+        "deliveryTime": 48,
+        "lastMileTravel": 11.8,
+        "serviceability": "SERVICEABLE",
+        "slaString": "45-50 mins",
+        "lastMileTravelString": "11.8 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2026-01-25 23:00:00",
+        "opened": true
+      },
+      "badges": {
+
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+
+          },
+          "textBased": {
+
+          },
+          "textExtendedBadges": {
+
+          }
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {
+
+        },
+        "subTitle": {
+
+        },
+        "message": {
+
+        },
+        "customIcon": {
+
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+
+          },
+          "video": {
+
+          }
+        }
+      },
+      "reviewsSummary": {
+
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-7dab50aa-5d32-4721-a5e3-601110ee7d93"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/chhindwara/asha-hotel-prasia-road-rest614453",
+      "text": "RESTAURANT_MENU",
+      "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+      "id": "1224503",
+      "name": "City Bakery",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/6/a21e6a96-ec7f-44ba-ba78-48b5b6a5a1e5_1224503.jpg",
+      "locality": "Chhindwara City",
+      "areaName": "Chhindwara City",
+      "costForTwo": "₹300 for two",
+      "cuisines": [
+        "Bakery"
+      ],
+      "avgRating": 2.2,
+      "parentId": "63360",
+      "avgRatingString": "2.2",
+      "totalRatingsString": "4",
+      "sla": {
+        "deliveryTime": 62,
+        "lastMileTravel": 11.1,
+        "serviceability": "SERVICEABLE",
+        "slaString": "60-70 mins",
+        "lastMileTravelString": "11.1 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2026-01-25 21:30:00",
+        "opened": true
+      },
+      "badges": {
+
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+
+          },
+          "textBased": {
+
+          },
+          "textExtendedBadges": {
+
+          }
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {
+
+        },
+        "subTitle": {
+
+        },
+        "message": {
+
+        },
+        "customIcon": {
+
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+
+          },
+          "video": {
+
+          }
+        }
+      },
+      "reviewsSummary": {
+
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "isNewlyOnboarded": true,
+      "restaurantOfferPresentationInfo": {
+
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-7dab50aa-5d32-4721-a5e3-601110ee7d93"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/chhindwara/city-bakery-chhindwara-city-rest1224503",
+      "text": "RESTAURANT_MENU",
+      "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+
+
+
 
 ]
 
